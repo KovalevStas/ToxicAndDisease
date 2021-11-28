dea35c18d21a7e3f408ca3c5f243705fa593675d
Станислав <stanislavkovalev.m@gmail.com>
2021-11-28 13:00:36 +0300

    Initial commit

---
+++ src/main/java/com/example/toxicanddisease/ToxicAndDiseaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.toxicanddisease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToxicAndDiseaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToxicAndDiseaseApplication.class, args);
+    }
+
+}
